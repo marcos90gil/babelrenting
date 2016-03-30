8493e526342d06024e23391150f243799d6847ce
Iván Manso <ivan.manso@babel-si.local>
2016-03-30 11:53:23 +0200

    Establecemos los values que utilizaremos para las rutas de la URL y las rutas de nuestra BBDD.

---
+++ scripts/values/paths.js
@@ -0,0 +1,9 @@
+angular.module("babelrenting").constant("paths", {
+    home: "/",
+    movies: "/movies",
+    movieNew: "/movies/new",
+    movieDetail: "/movies/:id",
+    movieRenter: "/movies/:renter",
+    movieOwner: "/movies/:owner",
+    notFound: "/sorry"
+});
